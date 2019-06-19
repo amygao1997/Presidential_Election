ca3b2e6234d7c3b04e4236c99eaf968f335fa5d1
omcomeau <ocomeau45@massmutual.com>
2019-06-19 14:57:12 -0400

    improved look

--- import_contrib_master.sql
+++ import_contrib_master.sql
@@ -4,37 +4,37 @@ CREATE TABLE contrib_from_cmtes
 --Contributions to candidates from committees info
 
 (
-    CMTE_ID VARCHAR(9) NOT NULL,
-    AMNDT_IND VARCHAR(1),
-    RPT_TP VARCHAR(3),
-    TRANSACTION_PGI VARCHAR(5),
-    IMG_NUM VARCHAR(18),
-    TRANSACTION_TP VARCHAR(3),
-    ENTITY_TP VARCHAR(3),
-    NAME VARCHAR(200),
-    CITY VARCHAR(30),
-    STATE VARCHAR(2),
-    ZIP_CODE VARCHAR(9),
-    EMPLOYER VARCHAR(38),
-    OCCUPATION VARCHAR(38),
-    TRANSACTION_DT DATE,
-    TRANSACTION_AMT NUMERIC(14,2),
-    OTHER_ID VARCHAR(9),
-    CAND_ID VARCHAR(9),
-    TRAN_ID VARCHAR(32),
-    FILE_NUM NUMERIC(22),
-    MEMO_CD VARCHAR(1),
-    MEMO_TEXT VARCHAR(100),
-    SUB_ID VARCHAR(19) NOT NULL,
+    cmte_id VARCHAR(9) NOT NULL,
+    amndt_ind VARCHAR(1),
+    rtp_tp VARCHAR(3),
+    transaction_pgi VARCHAR(5),
+    img_num VARCHAR(18),
+    transaction_tp VARCHAR(3),
+    entity_tp VARCHAR(3),
+    name VARCHAR(200),
+    city VARCHAR(30),
+    state VARCHAR(2),
+    zip_code VARCHAR(9),
+    employer VARCHAR(38),
+    occupation VARCHAR(38),
+    transaction_dt DATE,
+    transaction_amt NUMERIC(14,2),
+    other_id VARCHAR(9),
+    cand_id VARCHAR(9),
+    tran_id VARCHAR(32),
+    file_num NUMERIC(22),
+    memo_cd VARCHAR(1),
+    memo_text VARCHAR(100),
+    sub_id VARCHAR(19) NOT NULL,
     
-    CONSTRAINT contrib_from_cmtes_pkey PRIMARY KEY (CMTE_ID),
+    CONSTRAINT contrib_from_cmtes_pkey PRIMARY KEY (cmte_id),
     
-    CONSTRAINT contrib_from_cmtes_fkey_cand FOREIGN KEY (CAND_ID)
-    REFERENCES candidate_master_info (CAND_ID) MATCH SIMPLE
+    CONSTRAINT contrib_from_cmtes_fkey_cand FOREIGN KEY (cand_id)
+    REFERENCES candidate_master_info (cand_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION,
     
-    CONSTRAINT contrib_from_cmtes_fkey_cmte FOREIGN KEY (CMTE_ID)
+    CONSTRAINT contrib_from_cmtes_fkey_cmte FOREIGN KEY (cmte_id)
     REFERENCES public.cmte_master (cmte_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
