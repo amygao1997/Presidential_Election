55b929fb33503537065595a82cea846fdb07a078
Emily Dzwil <edzwil37@massmutual.com>
2019-06-19 14:44:47 -0400

    deleting old tables and consolidating into new

--- pres_elect_final.sql
+++ pres_elect_final.sql
@@ -1,8 +1,8 @@
 CREATE TABLE cand_to_cmte_linkage
 (
     cand_id VARCHAR(9) REFERENCES candidate_master_info(cand_id),
-    cand_election_yr SMALLINT,
-    fec_election_yr SMALLINT,
+    cand_election_yr smallint,
+    fec_election_yr smallint,
     cmte_id VARCHAR(9) REFERENCES cmte_master(cmte_id),
     cmte_tp VARCHAR(1),
     cmte_dsgn VARCHAR(1),
