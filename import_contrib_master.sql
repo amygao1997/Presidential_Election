e79eafe1e78461f410e30f58ac8f7ae3f09556b8
omcomeau <ocomeau45@massmutual.com>
2019-06-19 13:31:18 -0400

    makes? contributions to cand from cmtes table

---
+++ import_contrib_master.sql
@@ -0,0 +1,31 @@
+CREATE TABLE contrib_from_cmtes
+--Contributions to candidates from committees info
+
+(
+
+    CMTE_ID VARCHAR(9) NOT NULL,
+    AMNDT_IND VARCHAR(1),
+    RPT_TP VARCHAR(3),
+    TRANSACTION_PGI VARCHAR(5),
+    IMG_NUM VARCHAR(18),
+    TRANSACTION_TP VARCHAR(3),
+    ENTITY_TP VARCHAR(3),
+    NAME VARCHAR(200),
+    CITY VARCHAR(30),
+    STATE VARCHAR(2),
+    ZIP_CODE VARCHAR(9),
+    EMPLOYER VARCHAR(38),
+    OCCUPATION VARCHAR(38),
+    TRANSACTION_DT 
+    TRANSACTION_AMT NUMERIC(14,2),
+    OTHER_ID VARCHAR(9),
+    CAND_ID VARCHAR(9),
+    TRAN_ID VARCHAR(32),
+    FILE_NUM NUMERIC(22),
+    MEMO_CD VARCHAR(1),
+    MEMO_TEXT VARCHAR(100),
+    SUB_ID VARCHAR(19) NOT NULL,
+    CONSTRAINT contrib_from_cmtes_pkey PRIMARY KEY (CMTE_ID)
+    --CONSTRAINT contributions_from_cmtes_fkey_cand FOREIGN KEY (CAND_ID)
+    --REFERENCES candidate_master_info
+   )
