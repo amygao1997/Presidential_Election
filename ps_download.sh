eeb488a4c0958fa286fd8f61ea1c069f5ea36a9f
Amy Gao <amygao1997@gmail.com>
2019-06-20 10:47:15 -0400

    download script

---
+++ ps_download.sh
@@ -0,0 +1,10 @@
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/cn20.zip -P data
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/oppexp20.zip -P data
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/indiv20.zip -P data
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/pas220.zip -P data
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/oth20.zip -P data
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/ccl20.zip -P data
+wget --no-check-certificate https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2020/cm20.zip -P data
+
+unzip *.zip
+
