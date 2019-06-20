e355a9627f99e3cf362ad6172a9e8c8fa6814b25
omcomeau <ocomeau45@massmutual.com>
2019-06-20 10:52:48 -0400

    organization

--- import_contrib_master.sql
+++ import_contrib_master.sql
@@ -40,7 +40,4 @@ CREATE TABLE contrib_from_cmtes
     --ON DELETE NO ACTION
    );
     
-   \COPY contrib_from_cmtes 
-   FROM '/tmp/data/itpas2.txt' 
-   DELIMITER '|'
-   NULL ''
+   \COPY contrib_from_cmtes FROM '/tmp/data/itpas2.txt' DELIMITER '|' NULL ''
