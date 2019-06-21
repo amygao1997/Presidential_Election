aae43cec1e6f406f4cf5db6cf4bbc9ec80d09c49
Emily Dzwil <edzwil37@massmutual.com>
2019-06-21 12:30:38 -0400

    moving view

--- pres_elect_final.sql
+++ pres_elect_final.sql
@@ -214,13 +214,3 @@ WHERE cands.cand_office = 'P'
 AND cand_election_yr = '2020'
 AND cand_status = 'C'
 GROUP BY cmtes.cmte_id, cands.cand_id;
-
--- contributions to candidate committees from other committees
-DROP VIEW IF EXISTS pres2020_cmtes;
-CREATE VIEW pres2020_cmtes AS
-SELECT cands.cand_id, cmtes.cmte_id, sum(cc.transaction_amt) as cmtes_total
-FROM contrib_to_candidate_cmtes cc
-LEFT JOIN cands on cands.cand_id = cc.cand_id
-LEFT JOIN cmtes on cmtes.cmte_id = cc.cmte_id
-WHERE cand_election_yr ='2020' AND cand_office = 'P' and cand_status = 'C'
-GROUP BY cands.cand_id, cmtes.cmte_id;
