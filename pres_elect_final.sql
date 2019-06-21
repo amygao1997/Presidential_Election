49916eb1ebd32ee1174bdd58a58c8e7f8e903313
edwoodbrice <EBrice60@massmutual.com>
2019-06-21 12:23:41 -0400

    sql script change

--- pres_elect_final.sql
+++ pres_elect_final.sql
@@ -206,12 +206,11 @@ ALTER TABLE operating_expends DROP garbage;
 CREATE VIEW pres2020_indiv AS
 SELECT cmtes.cmte_id, 
    cands.cand_id,
-   sum(transaction_amt) AS amount
+   sum(transaction_amt) AS contrib_amount
 FROM contrib_by_indiv c
 LEFT JOIN cmtes ON c.cmte_id = cmtes.cmte_id
 LEFT JOIN cands ON cands.cand_id = cmtes.candidate_id
 WHERE cands.cand_office = 'P'
 AND cand_election_yr = '2020'
 AND cand_status = 'C'
-GROUP BY cmtes.cmte_id, cands.cand_id
-ORDER BY amount DESC;
+GROUP BY cmtes.cmte_id, cands.cand_id;
