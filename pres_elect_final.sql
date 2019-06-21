6b4e44eeead1e883e253269da93c9b534cef7860
edwoodbrice <EBrice60@massmutual.com>
2019-06-21 12:32:32 -0400

    deleted a view script

--- pres_elect_final.sql
+++ pres_elect_final.sql
@@ -201,16 +201,3 @@ CREATE TABLE operating_expends
 );
 \COPY operating_expends FROM '/tmp/data/oppexp.txt' WITH (DELIMITER '|', HEADER FALSE);
 ALTER TABLE operating_expends DROP garbage;
-
--- contributions from individuals
-CREATE VIEW pres2020_indiv AS
-SELECT cmtes.cmte_id, 
-   cands.cand_id,
-   sum(transaction_amt) AS contrib_total
-FROM contrib_by_indiv c
-LEFT JOIN cmtes ON c.cmte_id = cmtes.cmte_id
-LEFT JOIN cands ON cands.cand_id = cmtes.candidate_id
-WHERE cands.cand_office = 'P'
-AND cand_election_yr = '2020'
-AND cand_status = 'C'
-GROUP BY cmtes.cmte_id, cands.cand_id;
