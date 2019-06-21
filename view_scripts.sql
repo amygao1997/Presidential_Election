e2fb53a6db227b59496c480cbf57b429691484a5
Amy Gao <amygao1997@gmail.com>
2019-06-21 12:31:17 -0400

    add trasnactions table

--- view_scripts.sql
+++ view_scripts.sql
@@ -14,3 +14,21 @@ AND cand_election_yr = '2020'
 AND cand_status = 'C'
 GROUP BY cmtes.cmte_id, cands.cand_id;
 
+
+-- transactions total 
+
+DROP VIEW IF EXISTS res2020_trans;
+
+CREATE VIEW pres2020_trans AS 
+SELECT tl.cand_id, tl.cmte_id, SUM(transaction_amt) AS transaction_total
+FROM transactions t
+JOIN table_link tl ON (tl.cmte_id = t.cmte_id)
+WHERE t.cmte_id IN (SELECT cmte_id
+					FROM cmtes
+					WHERE cmte_id IN (SELECT cmte_id
+										FROM table_link
+											WHERE cand_id IN (SELECT cand_id
+				  		 							FROM cands
+					   								WHERE cand_election_yr='2020' AND cand_office= 'P' AND cand_status ='C'
+									)))
+GROUP BY tl.cand_id, tl.cmte_id ;
