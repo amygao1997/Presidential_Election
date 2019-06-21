aac2eecf145b06c264c1a6862862235768eec739
Emily Dzwil <edzwil37@massmutual.com>
2019-06-21 12:29:32 -0400

    naming convention for view

--- pres_elect_final.sql
+++ pres_elect_final.sql
@@ -216,8 +216,8 @@ AND cand_status = 'C'
 GROUP BY cmtes.cmte_id, cands.cand_id;
 
 -- contributions to candidate committees from other committees
-DROP VIEW IF EXISTS cmtes_total;
-CREATE VIEW cmtes_total AS
+DROP VIEW IF EXISTS pres2020_cmtes;
+CREATE VIEW pres2020_cmtes AS
 SELECT cands.cand_id, cmtes.cmte_id, sum(cc.transaction_amt) as cmtes_total
 FROM contrib_to_candidate_cmtes cc
 LEFT JOIN cands on cands.cand_id = cc.cand_id
